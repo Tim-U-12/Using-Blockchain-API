12fe06939d5a3de613bc79868df6b5c4a11e41fa
Tim <tim.a.ung.12@gmail.com>
2023-12-26 13:09:02 +1100

    Webpage is running on port 3000

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import axios from "axios"
 const app = express()
 const port = 3000
 
+app.get("/", async (req, res) => {
+    res.render("index.ejs")
+})
+
 app.listen(port, (err)=> {
     if (err) throw err
     console.log(`Webpage is running on port ${port}`)
