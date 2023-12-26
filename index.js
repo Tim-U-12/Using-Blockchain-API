837137957b3336fad48c06f77205f1fc0b92c77b
Tim <tim.a.ung.12@gmail.com>
2023-12-26 15:31:51 +1100

    use form to get ticker from user. then use api to get information on that ticker

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import express from "express"
 import axios from "axios"
+import bodyParser from "body-parser"
 
 const app = express()
 const port = 3000
+const API_URL = "https://api.blockchain.com/v3/exchange"
 
-app.get("/", async (req, res) => {
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/", (req, res) => {
     res.render("index.ejs")
+})
+
+app.post("/", async (req, res) => {
+    try {
+        const response = await axios(API_URL + "/symbols/" + req.body.ticker)
+        const result = response.data
+        res.render("index.ejs", {data: JSON.stringify(result, null, 4)})
+    } catch (error) {
+        console.error("Failed to make request:", error.message)
+    }
 })
 
 app.listen(port, (err)=> {
